096f54236b3df1e57afa5e51e1cda0c7cd353b29
barnaholl <hollbarna@freemail.hu>
2021-02-01 10:42:35 +0100

    init projct

---
+++ src/main/java/com/codecool/jokerchildspring/JokerChildSpringApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.jokerchildspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokerChildSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokerChildSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/jokerchildspring/JokerChildSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.jokerchildspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokerChildSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
