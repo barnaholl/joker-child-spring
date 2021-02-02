7991e8b1dd0cddfbbb4982d2381be8bd97789a49
VZL <info@ronexpress.hu>
2021-02-02 16:39:48 +0100

    make start project

--- src/main/java/com/codecool/jokerchildspring/controller/DummyController.java
+++ src/main/java/com/codecool/jokerchildspring/controller/DummyController.java
@@ -3,6 +3,7 @@ package com.codecool.jokerchildspring.controller;
 import com.codecool.jokerchildspring.entity.DummyEntity;
 import com.codecool.jokerchildspring.repository.DummyRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,10 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequiredArgsConstructor
 public class DummyController {
 
-    private final DummyRepository dummyRepository;
+    @Autowired
+    private DummyRepository dummyRepository;
 
     @GetMapping("/")
     public List<DummyEntity> getDummyEntities(){
