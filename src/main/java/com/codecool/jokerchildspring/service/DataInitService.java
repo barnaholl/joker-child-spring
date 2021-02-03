686790fd6b97f09f7505aec582f7cbc74b53cc8c
barnaholl <hollbarna@freemail.hu>
2021-02-03 10:56:12 +0100

    DataInitService Dummy data(Teacher,Student,School)

--- src/main/java/com/codecool/jokerchildspring/service/DataInitService.java
+++ src/main/java/com/codecool/jokerchildspring/service/DataInitService.java
@@ -1,15 +1,16 @@
 package com.codecool.jokerchildspring.service;
 
-import com.codecool.jokerchildspring.entity.Answer;
-import com.codecool.jokerchildspring.entity.Card;
-import com.codecool.jokerchildspring.entity.Exercise;
-import com.codecool.jokerchildspring.entity.Profession;
+import com.codecool.jokerchildspring.entity.*;
+import com.codecool.jokerchildspring.model.MemberRole;
 import com.codecool.jokerchildspring.repository.CardRepository;
+import com.codecool.jokerchildspring.repository.MemberRepository;
+import com.codecool.jokerchildspring.repository.SchoolRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,6 +19,8 @@ import java.util.List;
 public class DataInitService implements CommandLineRunner {
 
     private final CardRepository cardRepository;
+    private final SchoolRepository schoolRepository;
+    private final MemberRepository memberRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -33,5 +36,41 @@ public class DataInitService implements CommandLineRunner {
 
         Card card= Card.builder().identificationId("1001").profession(profession).exercise(exercise).build();
         cardRepository.save(card);
+
+        Member student= Member.builder()
+                .name("Dummy Student")
+                .nick("BestStudentEver")
+                .birthDate(Date.valueOf("1996-09-09"))
+                .email("best@student.com")
+                .password("1234")
+                .role(MemberRole.STUDENT)
+                .experience(0)
+                .build();
+
+
+
+        Member teacher= Member.builder()
+                .name("Dummy Teacher")
+                .nick("BestTeacherEver")
+                .birthDate(Date.valueOf("1996-09-09"))
+                .email("best@teacher.com")
+                .password("1234")
+                .role(MemberRole.TEACHER)
+                .experience(0)
+                .build();
+
+        memberRepository.save(teacher);
+        memberRepository.save(student);
+
+        School school= School.builder()
+                .city("Budapest")
+                .name("Average School")
+                .team("1a")
+                .student(student)
+                .teacher(teacher)
+                .build();
+
+        schoolRepository.save(school);
+
     }
 }
