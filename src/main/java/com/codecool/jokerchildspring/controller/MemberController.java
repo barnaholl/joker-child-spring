871a4f177dbab698c78991e9715766228f7ebecc
barnaholl <hollbarna@freemail.hu>
2021-02-22 13:46:03 +0100

    MemberController ResponseEntity type

--- src/main/java/com/codecool/jokerchildspring/controller/MemberController.java
+++ src/main/java/com/codecool/jokerchildspring/controller/MemberController.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @CrossOrigin
 @RestController
 @RequestMapping("/member")
@@ -16,46 +18,46 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/")
-    public ResponseEntity getMemberById(@RequestParam("id") Long id){
+    public ResponseEntity<Member> getMemberById(@RequestParam("id") Long id){
         return ResponseEntity.ok(memberService.getMemberById(id));
     }
 
     @GetMapping("/getXpByMemberId")
-    public ResponseEntity getXpByMemberId(@RequestParam("id") Long memberId){
+    public ResponseEntity<Long> getXpByMemberId(@RequestParam("id") Long memberId){
         return ResponseEntity.ok(memberService.getXpByMemberById(memberId));
     }
 
     @GetMapping("/all")
-    public ResponseEntity getAllMembers(){
+    public ResponseEntity<List<Member>> getAllMembers(){
         return ResponseEntity.ok(memberService.getAllMembers());
     }
 
     @PostMapping("/")
-    public ResponseEntity createMember(@ApiParam(value = "BirthDate format is XXXX-XX-XX(1996-09-09), possible Roles are : STUDENT,TEACHER,ADMIN ") @RequestBody Member member){
+    public ResponseEntity<String> createMember(@ApiParam(value = "BirthDate format is XXXX-XX-XX(1996-09-09), possible Roles are : STUDENT,TEACHER,ADMIN ") @RequestBody Member member){
         memberService.createMember(member);
         return ResponseEntity.ok("Member created"+member);
     }
 
     @PutMapping("/")
-    public ResponseEntity updateMember(@ApiParam(value = "BirthDate format is XXXX-XX-XX(1996-09-09), possible Roles are : STUDENT,TEACHER,ADMIN ") @RequestBody Member member){
+    public ResponseEntity<String> updateMember(@ApiParam(value = "BirthDate format is XXXX-XX-XX(1996-09-09), possible Roles are : STUDENT,TEACHER,ADMIN ") @RequestBody Member member){
         memberService.updateMember(member);
         return ResponseEntity.ok("Member updated to:"+member);
     }
 
-    @PutMapping("/putXp")
-    public ResponseEntity putXpToMember(@RequestParam("memberId") Long memberId,@RequestParam("experience") int experience){
-        memberService.putXpToMember(memberId,experience);
-        return ResponseEntity.ok("Member XP updated:"+memberId);
+    @PutMapping("/updateExperience")
+    public ResponseEntity<String> updateExperience(@RequestParam("memberId") Long memberId,@RequestParam("experience") Long experience){
+        memberService.updateExperience(memberId,experience);
+        return ResponseEntity.ok("Member's("+memberId+") experience updated to "+experience);
     }
 
     @DeleteMapping("/resetMember")
-    public ResponseEntity resetMember(@RequestParam("id") Long id){
+    public ResponseEntity<String> resetMember(@RequestParam("id") Long id){
         memberService.resetMember(id);
         return ResponseEntity.ok("Member reset with id: "+ id);
     }
 
     @DeleteMapping("/")
-    public ResponseEntity deleteMember(@RequestParam("id") Long id){
+    public ResponseEntity<String> deleteMember(@RequestParam("id") Long id){
         memberService.deleteMember(id);
         return ResponseEntity.ok("Member deleted with id: "+ id);
     }
