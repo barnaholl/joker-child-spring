868d02093d0b78d161ef52c780363922bf0911a0
barnaholl <hollbarna@freemail.hu>
2021-02-22 10:30:44 +0100

    CardController/ResponseEntity type

--- src/main/java/com/codecool/jokerchildspring/controller/CardController.java
+++ src/main/java/com/codecool/jokerchildspring/controller/CardController.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @CrossOrigin
 @RestController
 @RequestMapping("/card")
@@ -15,46 +17,46 @@ public class CardController {
     private final CardService cardService;
 
     @GetMapping("/")
-    public ResponseEntity getCardById(@RequestParam("id") Long id){
+    public ResponseEntity<Card> getCardById(@RequestParam("id") Long id){
         return ResponseEntity.ok(cardService.getCardById(id));
     }
 
     @GetMapping("/byIdentificationId")
-    public ResponseEntity getCardByIdentificationId(@RequestParam("identificationId") String identificationId){
+    public ResponseEntity<Card> getCardByIdentificationId(@RequestParam("identificationId") String identificationId){
         return ResponseEntity.ok(cardService.getCardByIdentificationId(identificationId));
     }
 
     @GetMapping("/all")
-    public ResponseEntity getAllCards(){
+    public ResponseEntity<List<Card>> getAllCards(){
         return ResponseEntity.ok(cardService.getAllCards());
     }
 
     @PostMapping("/")
-    public ResponseEntity createCard(@RequestBody  Card card){
+    public ResponseEntity<String> createCard(@RequestBody  Card card){
         cardService.createCard(card);
         return ResponseEntity.ok("Card created"+card);
     }
 
     @PostMapping("/withProfession")
-    public ResponseEntity createCardWithExistingProfession(@RequestParam("professionId") Long professionId,@RequestBody  Card card){
+    public ResponseEntity<String> createCardWithExistingProfession(@RequestParam("professionId") Long professionId,@RequestBody  Card card){
         cardService.createCardWithExistingProfession(card,professionId);
         return ResponseEntity.ok("Card created"+card+" with professionId:"+professionId);
     }
 
     @PutMapping("/withProfession")
-    public ResponseEntity updateCardWithExistingProfession(@RequestParam("professionId") Long professionId,@RequestBody  Card card){
+    public ResponseEntity<String> updateCardWithExistingProfession(@RequestParam("professionId") Long professionId,@RequestBody  Card card){
         cardService.updateCardWithExistingProfession(card,professionId);
         return ResponseEntity.ok("Card updated"+card+" with professionId:"+professionId);
     }
 
     @PutMapping("/")
-    public ResponseEntity updateCard(@RequestBody Card card){
+    public ResponseEntity<String> updateCard(@RequestBody Card card){
         cardService.updateCard(card);
         return ResponseEntity.ok("Card updated to:"+card);
     }
 
     @DeleteMapping("/")
-    public ResponseEntity deleteCard(@RequestParam("id") Long id){
+    public ResponseEntity<String> deleteCard(@RequestParam("id") Long id){
         cardService.deleteCard(id);
         return ResponseEntity.ok("Card deleted with id: "+ id);
     }
