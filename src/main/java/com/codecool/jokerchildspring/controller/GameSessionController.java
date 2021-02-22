d82162f612f7f0ce051a7f62691810984081c644
barnaholl <hollbarna@freemail.hu>
2021-02-22 13:28:17 +0100

    GameSession ResponseEntity type

--- src/main/java/com/codecool/jokerchildspring/controller/GameSessionController.java
+++ src/main/java/com/codecool/jokerchildspring/controller/GameSessionController.java
@@ -1,5 +1,6 @@
 package com.codecool.jokerchildspring.controller;
 
+import com.codecool.jokerchildspring.entity.Card;
 import com.codecool.jokerchildspring.entity.GameSession;
 import com.codecool.jokerchildspring.service.GameSessionService;
 import lombok.RequiredArgsConstructor;
@@ -16,30 +17,30 @@ public class GameSessionController {
     private final GameSessionService gameSessionService;
 
     @GetMapping("/")
-    public ResponseEntity getGameSessionByUserId(@RequestParam("userId") Long userId){
+    public ResponseEntity<GameSession> getGameSessionByUserId(@RequestParam("userId") Long userId){
         return ResponseEntity.ok(gameSessionService.getGameSessionByUserId(userId));
     }
 
     @PostMapping("/")
-    public ResponseEntity createGameSession(@RequestBody GameSession gameSession){
+    public ResponseEntity<String> createGameSession(@RequestBody GameSession gameSession){
         gameSessionService.createGameSession(gameSession);
         return ResponseEntity.ok("Game session created: "+ gameSession);
     }
 
     @Transactional
     @DeleteMapping("/")
-    public ResponseEntity deleteGameSessionByUserId(@RequestParam("userId") Long userId){
+    public ResponseEntity<String> deleteGameSessionByUserId(@RequestParam("userId") Long userId){
         gameSessionService.deleteGameSessionByUserId(userId);
         return ResponseEntity.ok("Game session deleted with userId: "+userId);
     }
 
     @GetMapping("/card/")
-    public ResponseEntity getGameSessionsCardByUserId(@RequestParam("userId") Long userId){
+    public ResponseEntity<Card> getGameSessionsCardByUserId(@RequestParam("userId") Long userId){
         return ResponseEntity.ok(gameSessionService.getGameSessionsCardByUserId(userId));
     }
 
     @GetMapping("/isActive")
-    public ResponseEntity getIsGameSessionActive(@RequestParam("userId") Long userId){
+    public ResponseEntity<Boolean> getIsGameSessionActive(@RequestParam("userId") Long userId){
         return ResponseEntity.ok(gameSessionService.getIsGameSessionActive(userId));
     }
 
