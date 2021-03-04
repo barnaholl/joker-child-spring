9b0bf86c55ae108d801e8b97592692ec3b5a12a6
barnaholl <hollbarna@freemail.hu>
2021-03-04 15:08:39 +0100

    refactor endpoints to use AuthService

--- src/main/java/com/codecool/jokerchildspring/security/AuthController.java
+++ src/main/java/com/codecool/jokerchildspring/security/AuthController.java
@@ -1,111 +1,39 @@
 package com.codecool.jokerchildspring.security;
 
 import com.codecool.jokerchildspring.entity.Member;
-import com.codecool.jokerchildspring.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.*;
 
-import javax.persistence.EntityNotFoundException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.time.Duration;
 import java.util.*;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final AuthenticationManager authenticationManager;
-    private final JwtTokenServices jwtTokenServices;
-    private final MemberRepository memberRepository;
-
-    public static final String TOKEN = "token";
-
+    private final AuthService authService;
 
     @PostMapping("/login")
-    public ResponseEntity login(@RequestBody UserCredentials data,HttpServletResponse response) {
-        try {
-            String username = data.getUsername();
-            String password= data.getPassword();
-
-            Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username,password));
-            List<String> roles = authentication.getAuthorities()
-                    .stream()
-                    .map(GrantedAuthority::getAuthority)
-                    .collect(Collectors.toList());
-
-
-            String token = jwtTokenServices.createToken(username, roles);
-
-            Map<Object, Object> model = new HashMap<>();
-            model.put("username", username);
-            model.put("roles", roles);
-            model.put("token", token);
-
-            addTokenToCookie(response,token);
-            return ResponseEntity.ok(model);
-        } catch (AuthenticationException e) {
-            throw new BadCredentialsException("Invalid username/password supplied");
-        }
+    public ResponseEntity<Map<Object,Object>> login(@RequestBody UserCredentials data,HttpServletResponse response) {
+        return ResponseEntity.ok(authService.login(data, response));
     }
 
     @PostMapping("/logout")
     public ResponseEntity<String> logout(HttpServletResponse response) {
-        createLogoutCookie(response);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(authService.logout(response));
     }
 
     @PostMapping("/register")
-    public ResponseEntity<String> register(HttpServletResponse response) {
-        createLogoutCookie(response);
-        return ResponseEntity.ok().build();
-    }
-
-    private void addTokenToCookie(HttpServletResponse response, String token) {
-        ResponseCookie cookie = ResponseCookie.from("token", token)
-                .domain("localhost") // should be parameterized
-                .sameSite("Strict")  // CSRF
-                .maxAge(Duration.ofHours(24))
-                .httpOnly(true)      // XSS
-                .path("/")
-                .build();
-        response.addHeader("Set-Cookie", cookie.toString());
-    }
-
-
-    private void createLogoutCookie(HttpServletResponse response) {
-        ResponseCookie cookie = ResponseCookie.from("token", "")
-                .domain("localhost") // should be parameterized
-                .sameSite("Strict")  // CSRF
-                .maxAge(0)
-                .httpOnly(true)      // XSS
-                .path("/")
-                .build();
-        response.addHeader("Set-Cookie", cookie.toString());
+    public ResponseEntity<String>  register(@RequestBody Member member) {
+        return ResponseEntity.ok(authService.register(member));
     }
 
     @GetMapping("/current-user-object")
-    public Member getCurrentUserObject(HttpServletRequest request) {
-        Optional<Cookie> jwtToken =
-                Arrays.stream(Optional.ofNullable(request.getCookies()).orElse(new Cookie[]{}))
-                        .filter(cookie -> cookie.getName().equals(TOKEN))
-                        .findFirst();
-        if (jwtToken.isPresent()) {
-            UsernamePasswordAuthenticationToken userToken = jwtTokenServices.validateTokenAndExtractUserSpringToken(jwtToken.get().getValue());
-            return memberRepository.findByName(userToken.getName()).orElseThrow(EntityNotFoundException::new);
-        }
-        return null;
+    public ResponseEntity<Member> getCurrentUserObject(HttpServletRequest request) {
+       return ResponseEntity.ok(authService.getCurrentUserObject(request));
     }
 
 }
